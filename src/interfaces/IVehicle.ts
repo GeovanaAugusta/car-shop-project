84efeeba24f1e1cbdd04e231edda6244a700f289
Geovana Augusta <geovanaag@hotmail.com>
2022-10-21 18:02:13 -0300

    feat: IVehicle Interface

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,10 @@
+export interface IVehicle {
+  model: string,
+  year: number,
+  color: string,
+  status?: boolean,
+  buyValue: number,
+}
+
+// export default IModel;
+// A interface deve ser exportada com o nome de IModel e não deve ser exportada de forma padrão.
